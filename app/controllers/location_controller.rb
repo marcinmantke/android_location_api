8f771ec1cc293a24e4884af880901915e562ee87
Marcin Mantke <marcin.mantke@naturaily.com>
2017-01-21 15:15:39 +0100

    Authorize location controller

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,6 +1,8 @@
 class LocationsController < BaseController
+  before_action :doorkeeper_authorize!
+
   def create
-    loc = Location.create(lat: params[:lat], lng: params[:lng], user_id: params[:user_id])
+    loc = Location.create(lat: params[:lat], lng: params[:lng], user_id: current_resource_owner.id)
     render json: loc.to_json, status: 201
   end
 end
